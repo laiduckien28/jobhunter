aed1fddb2e28f14b387a55c062475c02ac4b998d
kienld <laiduckien28@gmail.com>
2025-06-01 16:27:33 +0700

    Update Module Permission&Role

---
+++ src/main/java/vn/devops/jobhunter/util/constant/GenderEnum.java
@@ -0,0 +1,5 @@
+package vn.devops.jobhunter.util.constant;
+
+public enum GenderEnum {
+    FEMALE, MALE, OTHER
+}
---
+++ src/main/java/vn/devops/jobhunter/util/constant/ResumeStateEnum.java
@@ -0,0 +1,5 @@
+package vn.devops.jobhunter.util.constant;
+
+public enum ResumeStateEnum {
+    PENDING, REVIEWING, APPROVED, REJECTED
+}
